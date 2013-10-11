0983afcaf48e985aa2a87ec2cd4750934c022f9e
Artur Hebda <arturhebda@gmail.com>
2013-10-12 00:17:46 +0200

    [Rails4] Remove match from routes.rb.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
     :controller => 'clearance/sessions',
     :only       => [:new, :create, :destroy]
 
-  match 'sign_in'  => 'clearance/sessions#new', :as => 'sign_in'
-  match 'sign_out' => 'clearance/sessions#destroy', :via => :delete, :as => 'sign_out'
+  get 'sign_in'  => 'clearance/sessions#new', :as => 'sign_in'
+  delete 'sign_out' => 'clearance/sessions#destroy', :as => 'sign_out'
 end
